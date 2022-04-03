8c67da17b635386dae97d6c2bd3f26024735adc5
Harshdeep <harshdeepraghuwanshi91@gmail.com>
2022-04-03 21:27:24 +0530

    implementing server code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
-import { Express } from "express";
+import Express from 'express';
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors'; 
+import cors from 'cors';
+// import dotenv from 'dotenv';
 const app = Express();
+app.use(cors());
+// dotenv.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const PORT=process.env.PORT || 5000;
+const URL = process.env.URL;
+console.log(URL);
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+app.listen(PORT, () => {
+  console.log("Server is running on port : " + PORT);
+});
